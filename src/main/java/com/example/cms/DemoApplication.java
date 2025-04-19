7c8ce12141c613e9d13b2ffffd8ed7e4a2e01bb8
thakurbhagyashri <thakurbhagyshri078@gmail.com>
2025-04-19 14:18:29 +0500

    code to update the project structure

---
+++ src/main/java/com/example/cms/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello World!");
+	}
+//	@Bean
+//	public ModelMapper modelMapper(){
+//		return new ModelMapper();
+//	}
+}
