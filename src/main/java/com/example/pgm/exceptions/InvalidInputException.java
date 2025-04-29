04511e596bda507980df8b27223d5732a997de5a
thakurbhagyashri <thakurbhagyshri078@gmail.com>
2025-04-29 13:42:36 +0500

    code to update the .gitignore file

--- src/main/java/com/example/pgm/exceptions/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.example.pgm.exceptions;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/pgm/exceptions/OperationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.pgm.exceptions;
-
-public class OperationFailedException extends RuntimeException {
-    public OperationFailedException(String message) {
-        super(message);
-    }
-}
